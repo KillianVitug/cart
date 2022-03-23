0b7f5afddc008b776e81f6533f7b2bc111e65fd1
KillianVitug <killianjarelvitug@yahoo.com>
2022-03-23 22:03:21 +0800

    added a header and logo

--- shopping-cart.php
+++ shopping-cart.php
@@ -16,14 +16,25 @@ $shoppingCartItems = $shoppingCart->getAllItems();
 <html>
 <head>
     <title>My Cart</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="shopping-cart.jpeg" alt="" width="35" height="28" class="d-inline-block align-text-top">
+      Shopping Cart
+    </a>
+    <a class="navbar-brand 5rem">Welcome to your Shopping Cart! <?php echo $customer->getName() ?>!</a>
+    <form class="d-flex">
+    <h4 class="form-control me-2"> <a href="products-list.php">Shop more Products</a></h4>
+    </form>
+  </div>
+</nav>
 
-<h1>Welcome <?php echo $customer->getName() ?>!</h1>
-<h2>Shopping Cart</h2>
-<h4>
-    <a href="products-list.php">Shop More Products</a>
-</h4>
 
 <?php if (count($shoppingCartItems) > 0): ?>
 
